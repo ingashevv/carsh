c153df0ceea2966bace3e089476851b1f9269792
Daud <ingashevdaudino@gmail.com>
2023-07-05 16:26:32 +0300

    full project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+    });
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+
+    });
+
+    $('.menu__btn').on('click',function () {
+        $('.menu__list').toggleClass('.menu__list--active');
+    });
+
+
+});
